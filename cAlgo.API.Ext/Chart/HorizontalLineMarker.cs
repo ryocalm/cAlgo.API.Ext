f4e88a97d167e1453e7d6ba07e0513380bffb355
ryocalm <ryocalm@tutanota.com>
2023-02-04 18:06:45 +0900

    Add HorizontalLineMarker

---
+++ cAlgo.API.Ext/Chart/HorizontalLineMarker.cs
@@ -0,0 +1,22 @@
+﻿using System.Globalization;
+
+namespace cAlgo.API.Ext.Chart;
+
+public class HorizontalLineMarker
+{
+    public ChartEllipse Ellipse;
+    public double MidY;
+    public DateTime MidTime;
+    public string Name;
+
+    public HorizontalLineMarker(ChartEllipse ellipse, int symbolDigits)
+    {
+        Ellipse = ellipse;
+        MidY = Math.Round((ellipse.Y1 + ellipse.Y2) / 2.0, symbolDigits);
+
+        var timeDiff = ellipse.Time2 - ellipse.Time1;
+        MidTime = ellipse.Time1.AddTicks(timeDiff.Ticks / 2);
+        var midTimeDate = MidTime.ToString("MM/dd");
+        Name = $"marker@{MidY.ToString(CultureInfo.CurrentCulture)}({midTimeDate})";
+    }
+}
