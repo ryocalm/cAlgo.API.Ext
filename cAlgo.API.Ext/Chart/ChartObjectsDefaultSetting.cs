954b3f679e60cc127bb851041025b458746be769
ryocalm <ryocalm@tutanota.com>
2023-02-06 23:55:43 +0900

    Initial commit

---
+++ cAlgo.API.Ext/Chart/ChartObjectsDefaultSetting.cs
@@ -0,0 +1,304 @@
+﻿namespace cAlgo.API.Ext.Chart;
+
+public class ChartObjectsDefaultSetting
+{
+    private static API.Chart? _chart;
+    private static bool _observationMode;
+
+    public ChartObjectsDefaultSetting(API.Chart chart, bool observationMode)
+    {
+        _chart = chart;
+        _observationMode = observationMode;
+    }
+
+    private static void UpdateHorizontalLine(ChartObject obj)
+    {
+        if (obj is not ChartHorizontalLine horizontalLine)
+        {
+            return;
+        }
+
+        if (_chart == null)
+        {
+            return;
+        }
+
+        horizontalLine.SetColor(_chart.Symbol.Ask);
+        horizontalLine.SetDefault(_chart.TimeFrame);
+    }
+
+    private void UpdateVerticalLine(ChartObject obj)
+    {
+        if (obj is not ChartVerticalLine verticalLine)
+        {
+            return;
+        }
+
+        if (_chart == null)
+        {
+            return;
+        }
+
+        verticalLine.SetDefault(_chart.TimeFrame);
+    }
+
+    private void UpdateTrendLine(ChartObject obj)
+    {
+        if (obj is not ChartTrendLine trendLine)
+        {
+            return;
+        }
+
+        if (_chart == null)
+        {
+            return;
+        }
+
+        trendLine.SetColor(_chart.Symbol, _chart.Symbol.Ask);
+        trendLine.SetDefault(_chart.TimeFrame, _chart.Symbol);
+    }
+
+    private void UpdateRectangle(ChartObject obj)
+    {
+        if (obj is not ChartRectangle rectangle)
+        {
+            return;
+        }
+
+        if (_chart == null)
+        {
+            return;
+        }
+
+        // Y1 が上辺、Y2 が下辺となるように修正する。
+        if (rectangle.Y1 < rectangle.Y2)
+        {
+            (rectangle.Y2, rectangle.Y1) = (rectangle.Y1, rectangle.Y2);
+        }
+
+        // Time1 が左辺、Time2 が右辺となるように修正する。
+        if (rectangle.Time1 > rectangle.Time2)
+        {
+            (rectangle.Time2, rectangle.Time1) = (rectangle.Time1, rectangle.Time2);
+        }
+
+        // Rectangle の上下にある bar
+        var bars = _chart.Bars
+            .Where(bar => bar.OpenTime >= rectangle.Time1
+                          && bar.OpenTime <= rectangle.Time2)
+            .ToArray();
+
+        rectangle.SetColor(bars, _chart.Symbol.Ask, _chart.Symbol.Bid);
+        rectangle.SetDefault(_chart.TimeFrame);
+    }
+
+    private void UpdateEllipse(ChartObject obj)
+    {
+        if (obj is not ChartEllipse ellipse)
+        {
+            return;
+        }
+
+        if (_chart == null)
+        {
+            return;
+        }
+
+        ellipse.SetColor();
+        ellipse.SetDefault(_chart.TimeFrame);
+
+        // Ellipse を通る HorizontalLine
+        var hLines = _chart.FindAllObjects<ChartHorizontalLine>()
+            .Where(line => ellipse.Y2 < line.Y && line.Y < ellipse.Y1)
+            .ToArray();
+        ellipse.AdjustEllipseSize(hLines);
+    }
+
+    private void UpdateTriangle(ChartObject obj)
+    {
+        if (obj is not ChartTriangle triangle)
+        {
+            return;
+        }
+
+        if (_chart == null)
+        {
+            return;
+        }
+
+        triangle.SetColor();
+        triangle.SetDefault(_chart.TimeFrame);
+    }
+
+    private void UpdateText(ChartObject obj)
+    {
+        if (obj is not ChartText text)
+        {
+            return;
+        }
+
+        if (_chart == null)
+        {
+            return;
+        }
+
+        text.SetColor();
+        text.SetDefault(_chart.TimeFrame);
+    }
+
+    private void UpdateStaticText(ChartObject obj)
+    {
+        if (obj is not ChartStaticText staticText)
+        {
+            return;
+        }
+
+        if (_chart == null)
+        {
+            return;
+        }
+
+        staticText.SetColor();
+        staticText.SetDefault(_chart.TimeFrame);
+    }
+
+    private void UpdateIcon(ChartObject obj)
+    {
+        if (obj is not ChartIcon icon)
+        {
+            return;
+        }
+
+        if (_chart == null)
+        {
+            return;
+        }
+
+        icon.SetColor();
+        icon.SetDefault(_chart.TimeFrame);
+    }
+
+    /// <summary>
+    /// ChartObject 追加時に実行する。
+    /// 個々の object に対する処理。
+    /// </summary>
+    /// <param name="obj"></param>
+    public void OnAdded(ChartObject obj)
+    {
+        switch (obj.ObjectType)
+        {
+            case ChartObjectType.HorizontalLine:
+                UpdateHorizontalLine(obj);
+                break;
+
+            case ChartObjectType.VerticalLine:
+                UpdateVerticalLine(obj);
+                break;
+
+            case ChartObjectType.TrendLine:
+                UpdateTrendLine(obj);
+                break;
+
+            case ChartObjectType.Rectangle:
+                UpdateRectangle(obj);
+                break;
+
+            case ChartObjectType.Ellipse:
+                UpdateEllipse(obj);
+                break;
+
+            case ChartObjectType.Triangle:
+                UpdateTriangle(obj);
+                break;
+
+            case ChartObjectType.Text:
+                UpdateText(obj);
+                break;
+
+            case ChartObjectType.StaticText:
+                UpdateStaticText(obj);
+                break;
+
+            case ChartObjectType.Icon:
+                UpdateIcon(obj);
+                break;
+
+            case ChartObjectType.ArrowLine:
+                break;
+        }
+    }
+
+    /// <summary>
+    /// ChartObject 更新時に実行する。
+    /// 個々の object に対する処理。
+    /// </summary>
+    /// <param name="obj"></param>
+    public void OnUpdated(ChartObject obj)
+    {
+        if (_chart == null)
+        {
+            return;
+        }
+
+        switch (obj.ObjectType)
+        {
+            case ChartObjectType.HorizontalLine:
+                if (obj is not ChartHorizontalLine horizontalLine)
+                {
+                    return;
+                }
+
+                if (!_observationMode)
+                {
+                    horizontalLine.SetColor(_chart.Symbol.Ask);
+                }
+
+                horizontalLine.SetDefault(_chart.TimeFrame);
+                break;
+
+            case ChartObjectType.VerticalLine:
+                break;
+
+            case ChartObjectType.TrendLine:
+                if (obj is not ChartTrendLine trendLine)
+                {
+                    return;
+                }
+
+                if (!_observationMode)
+                {
+                    trendLine.SetColor(_chart.Symbol, _chart.Symbol.Ask);
+                }
+
+                trendLine.SetDefault(_chart.TimeFrame, _chart.Symbol);
+                break;
+
+            case ChartObjectType.Rectangle:
+                UpdateRectangle(obj);
+                break;
+
+            case ChartObjectType.Ellipse:
+                UpdateEllipse(obj);
+                break;
+
+            case ChartObjectType.Triangle:
+                UpdateTriangle(obj);
+                break;
+
+            case ChartObjectType.Text:
+                UpdateText(obj);
+                break;
+
+            case ChartObjectType.StaticText:
+                UpdateStaticText(obj);
+                break;
+
+            case ChartObjectType.Icon:
+                UpdateIcon(obj);
+                break;
+
+            case ChartObjectType.ArrowLine:
+                break;
+        }
+    }
+}
---
+++ cAlgo.API.Ext/Chart/ChartObjectsVisibilityExtensions.cs
@@ -0,0 +1,262 @@
+﻿namespace cAlgo.API.Ext.Chart;
+
+public static class ChartObjectsVisibilityExtensions
+{
+    /// <summary>
+    /// ChartObject を全て非表示にする。
+    /// </summary>
+    /// <param name="chart"></param>
+    public static void HideChartObjects(this API.Chart chart)
+    {
+        foreach (var horizontalLine in chart.FindAllObjects<ChartHorizontalLine>())
+        {
+            horizontalLine.IsHidden = true;
+        }
+
+        foreach (var verticalLine in chart.FindAllObjects<ChartVerticalLine>())
+        {
+            verticalLine.IsHidden = true;
+        }
+
+        foreach (var trendLine in chart.FindAllObjects<ChartTrendLine>())
+        {
+            trendLine.IsHidden = true;
+        }
+
+        foreach (var rectangle in chart.FindAllObjects<ChartRectangle>())
+        {
+            rectangle.IsHidden = true;
+        }
+
+        foreach (var ellipse in chart.FindAllObjects<ChartEllipse>())
+        {
+            ellipse.IsHidden = true;
+        }
+
+        foreach (var text in chart.FindAllObjects<ChartText>())
+        {
+            text.IsHidden = true;
+        }
+
+        foreach (var icon in chart.FindAllObjects<ChartIcon>())
+        {
+            icon.IsHidden = true;
+        }
+    }
+
+    /// <summary>
+    /// ChartHorizontalLine は全ての TimeFrame で表示する？
+    /// </summary>
+    public static void ShowHorizontalLines(this API.Chart chart)
+    {
+        var horizontalLines = chart.FindAllObjects<ChartHorizontalLine>();
+
+        if (!horizontalLines.Any())
+        {
+            return;
+        }
+
+        foreach (var horizontalLine in horizontalLines)
+        {
+            var addedTimeFrame = horizontalLine.Comment.ToTimeFrame();
+
+            if (addedTimeFrame >= chart.TimeFrame)
+            {
+                horizontalLine.IsHidden = false;
+            }
+
+            if (addedTimeFrame.IsLongerTermBy(chart.TimeFrame, 3))
+            {
+                horizontalLine.IsHidden = false;
+            }
+        }
+    }
+
+    /// <summary>
+    /// ChartVerticalLine
+    /// EconomicEvents のラインは H1 より短期でのみ表示する
+    /// </summary>
+    public static void ShowVerticalLines(this API.Chart chart)
+    {
+        var verticalLines = chart.FindAllObjects<ChartVerticalLine>();
+
+        if (!verticalLines.Any())
+        {
+            return;
+        }
+
+        foreach (var verticalLine in verticalLines)
+        {
+            if (verticalLine.IsEconomicEventsLine())
+            {
+                verticalLine.IsHidden = (chart.TimeFrame > TimeFrame.Hour);
+            }
+        }
+    }
+
+    /// <summary>
+    /// ChartRectangle
+    /// </summary>
+    public static void ShowRectangles(this API.Chart chart)
+    {
+        var rectangles = chart.FindAllObjects<ChartRectangle>();
+
+        if (!rectangles.Any())
+        {
+            return;
+        }
+
+        foreach (var rectangle in rectangles)
+        {
+            var addedTimeFrame = rectangle.Comment.ToTimeFrame();
+
+            // H4 以上で追加したものは M30 以上でのみ表示する
+            if (addedTimeFrame >= TimeFrame.Hour4)
+            {
+                if (chart.TimeFrame >= TimeFrame.Minute30)
+                {
+                    rectangle.IsHidden = false;
+                }
+            }
+
+            // M5 以下で追加したものは H1 以下でのみ表示する
+            if (addedTimeFrame > TimeFrame.Minute5)
+            {
+                continue;
+            }
+
+            if (chart.TimeFrame <= TimeFrame.Hour)
+            {
+                rectangle.IsHidden = false;
+            }
+        }
+    }
+
+    /// <summary>
+    /// ChartTrendLine
+    /// </summary>
+    public static void ShowTrendLines(this API.Chart chart)
+    {
+        var trendLines = chart.FindAllObjects<ChartTrendLine>();
+
+        if (!trendLines.Any())
+        {
+            return;
+        }
+
+        foreach (var trendLine in trendLines)
+        {
+            var addedTimeFrame = trendLine.Comment.ToTimeFrame();
+
+            // 水平部分線の場合
+            if (trendLine.IsHorizontalPartialLine(chart.Symbol))
+            {
+                // 追加時の TimeFrame および上 3 段階まで表示する
+                if (addedTimeFrame.IsLongerTermBy(chart.TimeFrame, 3))
+                {
+                    trendLine.IsHidden = false;
+                }
+            }
+            // それ以外の場合は追加時の TimeFrame でのみ表示する
+            else
+            {
+                if (addedTimeFrame == chart.TimeFrame)
+                {
+                    trendLine.IsHidden = false;
+                }
+            }
+        }
+    }
+
+    /// <summary>
+    /// ChartEllipse
+    /// 追加時の TimeFrame より短期、および 3 段階長期まで表示する
+    /// </summary>
+    public static void ShowEllipses(this API.Chart chart)
+    {
+        var ellipses = chart.FindAllObjects<ChartEllipse>();
+
+        if (!ellipses.Any())
+        {
+            return;
+        }
+
+        foreach (var ellipse in ellipses)
+        {
+            var addedTimeFrame = ellipse.Comment.ToTimeFrame();
+
+            if (chart.TimeFrame <= addedTimeFrame)
+            {
+                ellipse.IsHidden = false;
+            }
+
+            if (addedTimeFrame.IsLongerTermBy(chart.TimeFrame, 3))
+            {
+                ellipse.IsHidden = false;
+            }
+        }
+    }
+
+    /// <summary>
+    /// ChartText
+    /// 追加時の TimeFrame と同じ TimeFrame なら表示する
+    /// </summary>
+    public static void ShowTexts(this API.Chart chart)
+    {
+        var texts = chart.FindAllObjects<ChartText>();
+
+        if (!texts.Any())
+        {
+            return;
+        }
+
+        foreach (var text in texts)
+        {
+            var addedTimeFrame = text.Comment.ToTimeFrame();
+
+            if (addedTimeFrame == chart.TimeFrame)
+            {
+                text.IsHidden = false;
+            }
+        }
+    }
+
+    /// <summary>
+    /// ChartIcon
+    /// 追加時の TimeFrame と同じ TimeFrame なら表示する
+    /// </summary>
+    public static void ShowIcons(this API.Chart chart)
+    {
+        var icons = chart.FindAllObjects<ChartIcon>();
+
+        if (!icons.Any())
+        {
+            return;
+        }
+
+        foreach (var icon in icons)
+        {
+            var addedTimeFrame = icon.Comment.ToTimeFrame();
+
+            if (addedTimeFrame == chart.TimeFrame)
+            {
+                icon.IsHidden = false;
+            }
+        }
+    }
+
+    /// <summary>
+    /// 現在の TimeFrame で表示するべき ChartObject を表示する。
+    /// </summary>
+    /// <param name="chart"></param>
+    public static void ShowChartObjects(this API.Chart chart)
+    {
+        chart.ShowHorizontalLines();
+        chart.ShowVerticalLines();
+        chart.ShowRectangles();
+        chart.ShowTrendLines();
+        chart.ShowEllipses();
+        chart.ShowTexts();
+        chart.ShowIcons();
+    }
+}
